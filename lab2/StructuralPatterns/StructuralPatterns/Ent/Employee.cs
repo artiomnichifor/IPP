3dca537f81797a541ddf0b34814f55d7da5f7b64
artiomnichifor <nick.art08@gmail.com>
2018-11-29 00:35:08 +0200

    lab2

---
+++ lab2/StructuralPatterns/StructuralPatterns/Ent/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuralPatterns.Ent
+{
+    public abstract class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime DateOfEmployment { get; set; } = DateTime.Now;
+
+    }
+}
